eb8d2c6cec648a1f24bf3aff1f78cd7279f20802
SCHUTJER <9685495@graniteschools.org>
2019-07-09 09:18:27 -0600

    Enter

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 import PorfolioContainer from './portfolio/portfolio-container';
+import PorfolioContainer from './NavigationContainer/navigation-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Stella Schutjer Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PorfolioContainer />
