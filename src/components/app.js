073d460ca134f95d8880ad7bef551ebf84ee9370
SCHUTJER <9685495@graniteschools.org>
2019-07-09 11:04:59 -0600

    enter

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from './pages/contact';
-import PortfolioDetail from './pages/portfolio-detail';
+import PortfolioDetail from './portfolio/portfolio-detail';
 
 export default class App extends Component {
   render() {
