68309b9f2256a4a246111ca1f890df7b02b9f46f
SCHUTJER <9685495@graniteschools.org>
2019-07-11 10:38:12 -0600

    enter

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function(props) {
-    const { id, description, thumb_image_url, logo_url} = props.item;
-    return (
-        <div className="portfolio-item-wrapper">
-            <div
-                className="portfolio-img-background"
-                style={{
-                    backgroundImage: "url(" + thumb_image_url + ")"
-                }}
-            />
-            
-            <div className="img text-wrapper">
-                <div className="logo-wrapper">
-                    <img src={logo_url} />
-                </div>
+export default class PortfolioItem extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-                <div className="subtitle">
-                    {description}
+    render() {
+        const { id, description, thumb_image_url, logo_url} = this.props.item;
+        return (
+            <div className="portfolio-item-wrapper">
+                <div
+                    className="portfolio-img-background"
+                    style={{
+                        backgroundImage: "url(" + thumb_image_url + ")"
+                    }}
+                />
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src={logo_url} />
+                    </div>
+
+                    <div className="subtitle">
+                        {description}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
