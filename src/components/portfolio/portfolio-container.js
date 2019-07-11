abf588c34ca3152f7ef24c5953331e1fd215fd13
SCHUTJER <9685495@graniteschools.org>
2019-07-11 11:03:23 -0600

    enter

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -53,23 +53,17 @@ export default class PortfolioContainer extends Component {
         }
 
         return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-            
-                <button onClick={() => this.handleFilter('0data')}>
-                    0data
-                </button>
-                <button onClick={() => this.handleFilter('1data')}>
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter('0data')}>
+                    0data                </button>
+                <button className="btn" onClick={() => this.handleFilter('1data')}>
                     1data
                 </button>
-                <button onClick={() => this.handleFilter('2data')}>
+                <button className="btn" onClick={() => this.handleFilter('2data')}>
                     2data
                 </button>
-                <button onClick={() => this.handleFilter('3data')}>
-                    3data
-                </button>
 
-                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+                {this.portfolioItems()}
             </div>
         );
     }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -4,14 +4,32 @@ import { Link } from 'react-router-dom';
 export default class PortfolioItem extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            portfolioItemClass: ""
+        };
+    }
+
+    handleMouseEnter() {
+        this.setState({portfolioItemClass: "image-blur" });
+    }
+
+    handleMouseLeave() {
+        this.setState({portfolioItemClass: ""});
     }
 
     render() {
         const { id, description, thumb_image_url, logo_url} = this.props.item;
         return (
-            <div className="portfolio-item-wrapper">
+            <div 
+            className="portfolio-item-wrapper"
+            onMouseEnter={() => this.handleMouseEnter()}
+            onMouseLeave={() => this.handleMouseLeave()}
+           >
                 <div
-                    className="portfolio-img-background"
+                    className={
+                        "portfolio-img-background " + this.state.portfolioItemClass
+                    }
                     style={{
                         backgroundImage: "url(" + thumb_image_url + ")"
                     }}
