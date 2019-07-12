f947c27a013a526484f032f211782929fa3c6df6
SCHUTJER <9685495@graniteschools.org>
2019-07-12 08:52:57 -0600

    enter

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Login from '../auth/login';
+import login from '../auth/login';
 import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
@@ -15,7 +15,7 @@ export default class Auth extends Component {
 
 
             <div className="right-column">
-                <Login />
+                <login />
             </div>
         </div>
         );
