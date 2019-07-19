329b32bae320d85637d67451f94f86f97025af0a
SCHUTJER <9685495@graniteschools.org>
2019-07-19 10:08:22 -0600

    enter

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BlogForm extends Component {
+    render() {
+        return (
+            <form>
+                <input type="text"/>
+                <input type="text"/>
+            </form>
+        );
+    }
+}
